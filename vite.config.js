ed688a20db1a074ad73ef7640b5203ee387597db
Nadia Naz <nadi.naz.10041@gmail.com>
2024-12-13 19:34:07 +0500

    merge

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
 export default defineConfig({
   plugins: [react()],
   root: './', // Ensure this matches the project root
@@ -10,9 +9,4 @@ export default defineConfig({
       input: '/src/main.jsx', // Ensure this points to the correct entry point
     },
   },
-=======
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
->>>>>>> a58244a20e464d652423458d4bbd32831a7ce5ef
 })
