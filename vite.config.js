610dd5f2d235f3a1e2505012476dcd23e031f85b
Nadia Naz <nadi.naz.10041@gmail.com>
2024-12-13 19:59:14 +0500

    resolve error

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './', // Ensure this matches the project root
-  build: {
-    rollupOptions: {
-      input: '/src/main.jsx', // Ensure this points to the correct entry point
-    },
-  },
 })
